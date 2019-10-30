caa0824bb5626ec8d7f31ccf82842ce009301434
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-10-30 10:59:39 +0200

    h3init

---
+++ harjotukset/h3.js
@@ -0,0 +1,28 @@
+function init() {
+    $("#lar").click(animatefun);
+}
+
+function imghide() {
+    $("img").animate(
+        { opacity: 0.0 }
+    );
+}
+
+function imgshow() {
+    $("img").animate(
+        { opacity: 0.5 }
+    );
+}
+
+function animatefun() {
+
+    $(".box").animate({ width: "300px" });
+    $(".box").animate({ height: "300px" });
+    $(".box").animate({ marginLeft: "150px" });
+    $(".box").animate({ borderWidth: "10px" });
+    $(".box").animate({ opacity: 0.5 });
+    $("img").mouseenter(imghide);
+    $("img").mouseleave(imgshow);
+}
+
+$(document).ready(init);
