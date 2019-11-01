568c24d08b8fe8dc5854184fc5e06b31a2256780
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-11-01 11:01:43 +0200

    h4v2 init

---
+++ harjotukset/h4v2/h4.js
@@ -0,0 +1,8 @@
+function hideshow(a) {
+    var x = document.getElementById(a);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
