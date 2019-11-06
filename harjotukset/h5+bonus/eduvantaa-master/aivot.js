7e7e50adb42797d5f3804b561b72f23cd9a4f2cc
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-11-06 10:02:46 +0200

    kohdat 1- 6 toteutus

--- harjotukset/h5+bonus/eduvantaa-master/aivot.js
+++ harjotukset/h5+bonus/eduvantaa-master/aivot.js
@@ -1,25 +1,46 @@
-function laatikkoFunktio(boxNumber){
-    alert("Nyt vaihdetaan laatikko"+ boxNumber + ":n sisältöä!");
+function laatikkoFunktio(bx){
+    //alert("Nyt vaihdetaan laatikko"+ bx + ":n sisältöä!");
+    vaihda();
 
-    if (boxNumber == 1){
+    if (bx == 1){
         document.getElementById("laatikko1").innerHTML = "<h1 class='uusiOtsikko1'>Tähän tulee uusi otsikko!</h1>" + 
         "<p>Ja tähän tietysti jotain sisältöä...</p>";
         document.getElementById("laatikko1").style.lineHeight = "50px";
     }
-    else if (boxNumber == 2){
-        // toiminnallisuus puuttuu!
+    else if (bx == 2){
+        document.getElementById("latiko2").innerHTML = "<ul><li>e</li><li>e</li><li>e</li><li>e</li><li>e</li><li>e</li><li>e</li></ul>";
+    }
+    else if (bx == 3){
+        document.getElementById("latikko3").innerHTML = '<img src="smiley1.png" ></img>';
     }
     else{
-        //toiminnallisuus puuttuu!
+        console.log("error invalid argument to 'laatikkoFunktio':" + bx + "\n");
     }
     
 }
 function yhteystietoFunktio() {
     var x = document.getElementById("yhteystietolomake");
+    var y = ["Nimi: ", "Email: ", "Puh: "];
     var text = "";
     var i;
-    for (i = 0; i < x.length; i++) {
-      text += x.elements[i].value + "<br>";
+    for (i = x.length - 1; i >= 0; i--) {
+      text += y[i] + x.elements[i].value + "<br>";
+      //console.log(i);
     }
     document.getElementById("yhteystiedotTuloste").innerHTML = text;
+  }
+
+  var state = 0;
+function vaihda(){
+    if(state == 0){
+        document.getElementById("img").setAttribute("src","smiley2.png");
+        state = 1;
+    }
+    else if(state == 1){
+        document.getElementById("img").setAttribute("src","smiley1.png");
+        state = 0;
+    }
+
+
+
   }
