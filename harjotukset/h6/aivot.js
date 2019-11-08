26b770d494aee92fb0b424ecf06ce2b22f69e034
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-11-08 10:33:40 +0200

    h6

--- harjotukset/h6/aivot.js
+++ harjotukset/h6/aivot.js
@@ -21,11 +21,11 @@ function laatikkoFunktio(bx){
 
 function yhteystietoFunktio() {
     var x = document.getElementById("yhteystietolomake");
-    //var y = ["Nimi: ", "Email: ", "Puh: "];
+    
     var text = "";
     var i;
     for (i = x.length - 1; i >= 0; i--) {
-      text += x.elements[i].name + ": "+ x.elements[i].value + "<br>";
+      text += x.elements[i].name + ": " + x.elements[i].value + "<br>";
       //console.log(i);
     }
     document.getElementById("yhteystiedotTuloste").innerHTML = text;
@@ -45,3 +45,32 @@ function vaihda(){
 
 
 }
+
+function diagrammiFunktio(item_arr=[45,43,74,78,85,65,85,63,25]){
+    // etsitään harjoitus2.html sivulta elementti, jonka id=myCanvas
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    //var item=[45,43,74,78,85,65,85,63,25];
+    var j = 0;
+    
+    // diagrammiin voidaan kirjoittaa numeroita haluttuihin kohtiin.
+    
+    for (let i = 0; i <= c.width; i += 20) {
+        ctx.fillText(i, i, c.height);
+        if(j < item_arr.length){
+            if(i==0){
+                ctx.moveTo(0,item_arr[j]);
+            }
+            else{
+                ctx.lineTo(i, item_arr[j]);
+            }
+            j++;
+        }
+        
+    }//for
+
+    ctx.stroke();
+    
+
+} 
