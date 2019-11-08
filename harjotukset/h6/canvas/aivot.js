401dd6665866ca3519724c7f0e333e5e05621ea6
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-11-08 12:51:33 +0200

    aded baisi functiuns

--- harjotukset/h6/canvas/aivot.js
+++ harjotukset/h6/canvas/aivot.js
@@ -1,4 +1,44 @@
 
+var draw_buf = {
+    "x_lable":[],
+    "y_lable":[],
+    "dataarr":{},
+    "datalable":[],
+    "dataarrcolor":[],
+    "options":null
+}
+//lable = "name":    //nuber 1 foor cords 
+//      "position": [x,y] //number 1 foor corespondig data points coordinates
+
+//dataarr = "name":
+//          "data":[x,y]
+
+function generatedata(arr,spase,name){
+    for(var i=0;i<arr.length;i++){
+        draw_buf.dataarr.push({"name":name ,"data":[arr[i],i*spase]});
+    }
+}
+
+function generatexy(xval,yval,xspase=20,yspase=20){
+    for(var i=0;i<xval.length;i++){
+        draw_buf.x_lable.push([xval[i],i*xspase]);
+    }
+    for(var i=0;i<yval.length;i++){
+        draw_buf.y_lable.push([yval[i],i*yspase]);
+    }
+}
+
+function deletedata(dataname){
+    for(var i = 0;i<draw_buf.dataarr.length;i++){
+        if(draw_buf.dataarr[i].name == dataname){
+            draw_buf.dataarr[i].splice(i,1);
+        }
+    }
+    
+}
+
+
+
 function diagrammiFunktio(item_arr=[45,43,74,78,85,65,85,63,25]){
     // etsitään harjoitus2.html sivulta elementti, jonka id=myCanvas
     var c = document.getElementById("myCanvas");
@@ -31,5 +71,5 @@ function diagrammiFunktio(item_arr=[45,43,74,78,85,65,85,63,25]){
 } 
 
 function clear(){
-    
+
 }
