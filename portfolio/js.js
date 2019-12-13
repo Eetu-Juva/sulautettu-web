ece2f1eaa09b8833cd78d0ee7448538ced2b0744
eetuj-koullu <56993574+eetuj-koullu@users.noreply.github.com>
2019-12-13 09:49:59 +0200

    idk

--- portfolio/js.js
+++ portfolio/js.js
@@ -1,4 +1,20 @@
+var isimagehiden=0;
+
+
+//canges the bacround color
 function change(){
     var color = document.getElementById("colorcage").value; 
     document.getElementById("page").style.backgroundColor=color;
+}
+
+//hide/sow profile picture
+function fadeimg(){
+    if (isimagehiden) {
+        $("#image").show(1000);
+        isimagehiden=0;  
+    }
+    else{
+        $("#image").hide(1000);
+        isimagehiden=1;
+    }    
 }
